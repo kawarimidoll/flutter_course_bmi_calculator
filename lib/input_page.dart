4cc1b96ce44341b251bf21ed15e4daefde77a90d
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-25 20:48:21 +0900

    add widget ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,9 +12,50 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: ReusableCard(Color(0xFF1D1E33)),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ReusableCard(Color(0xFF1D1E33)),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: ReusableCard(Color(0xFF1D1E33)),
+              ),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  ReusableCard(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: 200.0,
     );
   }
 }
