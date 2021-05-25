13c7343a57c1d57b403f1e7641482748a07ae186
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-25 21:13:34 +0900

    refactor with child widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -21,23 +22,44 @@ class _InputPageState extends State<InputPage> {
           Row(
             children: [
               Expanded(
-                child: ReusableCard(activeCardColor),
+                child: ReusableCard(
+                  color: activeCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                ),
               ),
               Expanded(
-                child: ReusableCard(activeCardColor),
+                child: ReusableCard(
+                  color: activeCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
+                ),
               ),
             ],
           ),
           Expanded(
-            child: ReusableCard(activeCardColor),
+            child: ReusableCard(
+              color: activeCardColor,
+              child: Container(),
+            ),
           ),
           Row(
             children: [
               Expanded(
-                child: ReusableCard(activeCardColor),
+                child: ReusableCard(
+                  color: activeCardColor,
+                  child: Container(),
+                ),
               ),
               Expanded(
-                child: ReusableCard(activeCardColor),
+                child: ReusableCard(
+                  color: activeCardColor,
+                  child: Container(),
+                ),
               ),
             ],
           ),
@@ -53,13 +75,41 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  IconContent({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80.0),
+        SizedBox(height: 15.0),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color color;
-  ReusableCard(this.color);
+  final Widget child;
+
+  ReusableCard({required this.color, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: child,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: color,
